974e6fce8a4dab6fd6d916e6c121f0b67b7c0930
sarath <sarathatit@gmail.com>
2020-09-22 17:07:24 +0530

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by sarath kumar on 22/09/20.
+//  Copyright © 2020 sarath kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
